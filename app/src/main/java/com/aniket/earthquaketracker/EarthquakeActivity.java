6214c87734dc8693ceda54d2bcb0d81aa9f18656
Aniket Chaudhri <anichaudhri@gmail.com>
2021-08-19 10:40:21 +0530

    Add Check for Internet and Earthquake Count

--- app/src/main/java/com/aniket/earthquaketracker/EarthquakeActivity.java
+++ app/src/main/java/com/aniket/earthquaketracker/EarthquakeActivity.java
@@ -13,7 +13,10 @@ import android.app.LoaderManager.LoaderCallbacks;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,6 +25,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,12 +47,18 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
     private String USGS_REQUEST_URL;
     private EarthquakeAdapter adapter;
+    private TextView txtNoEarthquakes;
+    private ProgressBar loadingSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
+
+
         String minMagnitude = getIntent().getStringExtra(MIN_MAGNITUDE);
         String earthquakeCount = getIntent().getStringExtra(EARTHQUAKE_COUNT);
+        txtNoEarthquakes = findViewById(R.id.txtNoEarthquakes);
+        loadingSpinner = findViewById(R.id.loadingSpinner);
 
         Log.v("EarthquakeActivity", "Input received " + minMagnitude);
         USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmagnitude=" + minMagnitude + "&limit=" + earthquakeCount;
@@ -99,12 +110,19 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
             }
         });
+        earthquakeListView.setEmptyView(txtNoEarthquakes);
 
 //        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
 //        task.execute(USGS_REQUEST_URL);
 
-        androidx.loader.app.LoaderManager loaderManager = getSupportLoaderManager();
-        loaderManager.initLoader(1, null, EarthquakeActivity.this);
+        if (isNetworkAvailable()) {
+            androidx.loader.app.LoaderManager loaderManager = getSupportLoaderManager();
+            loaderManager.initLoader(1, null, EarthquakeActivity.this);
+        }else{
+            loadingSpinner.setVisibility(View.GONE);
+
+            txtNoEarthquakes.setText("Check your Internet Connection");
+        }
     }
 
     @NonNull
@@ -115,15 +133,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
     @Override
     public void onLoadFinished(@NonNull Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        loadingSpinner.setVisibility(View.GONE);
         adapter.clear();
         if (data != null && !data.isEmpty()){
             adapter.addAll(data);
         }
+
+        txtNoEarthquakes.setText("No Earthquakes Found");
+
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<List<Earthquake>> loader) {
         adapter.clear();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 //    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>>{
